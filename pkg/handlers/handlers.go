02630cca966915ebde10333fbfaefbad79124aea
aipengLi <2333850040c@hiro.kindai.ac.jp>
2023-11-24 21:07:49 +0900

    Initial commit

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"github.com/aihou/bookings/pkg/config"
+	"github.com/aihou/bookings/pkg/render"
+	"github.com/aihou/bookings/pkg/models"
+	"net/http"
+)
+
+
+
+// Repo is the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is a function that returns the home page
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl.html", &models.TemplateData{})
+}
+
+// About is a function that returns the about page
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	//perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+
+	stringMap["remote_ip"] = remoteIP
+
+	//send the data to the template
+	render.RenderTemplate(w, "about.page.tmpl.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
---
+++ pkg/render/render.go
@@ -0,0 +1,104 @@
+package render
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"github.com/aihou/bookings/pkg/models"
+	"github.com/aihou/bookings/pkg/config"
+)
+
+//var functions = template.FuncMap{}
+
+var app *config.AppConfig
+
+// Newtemplates sets the config for the template package
+func Newtemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+// RenderTemplate renders templates using html/template
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+	if app.UseCache {
+		//get the template cache from the app config
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("could not get template from template cache")
+	}
+
+	//这里是创建一个缓冲区，然后将模板渲染到缓冲区中
+	buf := new(bytes.Buffer)
+
+	//这里的Execute的作用是将模板渲染到缓冲区中
+	_ = t.Execute(buf, td)
+
+	//render the template
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		log.Println("Error writing template to browser", err)
+	}
+}
+
+// createTemplateCache函数是创建一个模板缓存，这里的map的key是string，value是*template.Template
+// 其目的为了提高性能，因为如果每次请求都要解析模板文件，那么性能就会很低，所以我们可以将模板文件
+// 解析到模板中，然后将模板存入到map中，这样就可以提高性能了
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	//initialize a new map to act as the cache
+	myCache := map[string]*template.Template{}
+
+	//get all of the files named *.page.tmpl.html from ./templates
+	pages, err := filepath.Glob("./templates/*.page.tmpl.html")
+	if err != nil {
+		return myCache, err
+	}
+
+	//range through all files ending with *.page.tmpl.html
+	for _, page := range pages {
+		//extract the file name
+		//filepath.Base的作用是返回路径的最后一个元素，比如说
+		//./templates/home.page.tmpl.html，返回的就是home.page.tmpl.html
+		//注意这里的filepath.Base函数只拿出文件名，不拿出内容
+		name := filepath.Base(page)
+
+		//这里的template.New(name)是创建一个新的模板，然后用ParseFiles方法，
+		//将模板文件解析到模板中，这里的name是模板的名字，也就是home.page.tmpl.html
+		//这里的ts是一个指向模板的指针
+		//ParseFiles的作用是将模板文件解析到模板中，比如说，我们有一个模板文件
+		//home.page.tmpl.html，然后我们用ParseFiles方法将这个模板文件解析到模板中，
+		//然后我们就可以用这个模板来渲染页面了
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl.html")
+		if err != nil {
+			return myCache, err
+		}
+		if len(matches) > 0 {
+			//ParseGlob的作用是将模板文件解析到模板中，这里的*是通配符，表示匹配所有的文件
+			//比如说，我们有一个模板文件home.layout.tmpl.html，然后我们用ParseGlob方法将
+			//这个模板文件解析到模板中，然后我们就可以用这个模板来渲染页面了
+			//ts可以存入多个模板吗？可以的，ts是一个指向模板的指针，我们可以用ts来存入多个模板
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl.html")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		//名称对应内容，比如说home.page.tmpl.html对应的是ts
+		myCache[name] = ts
+	}
+	return myCache, nil
+}
